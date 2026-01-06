04efbf5532bcc3fae4a84572776a9eafe66be06f
Denys Abrashyn <denis.abrashin@gmail.com>
2026-01-06 15:32:32 +0200

    HW 15.1. Error Handling

---
+++ tests/homework15.test.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+
+const baseURL = 'https://jsonplaceholder.typicode.com';
+
+async function invalidUrl(url) {
+    try {
+        await axios.get(`${url}/invalid`);
+    } catch (error) {
+        if (error.response) {
+            return error.response.status;
+        }
+        return error.message;
+    }
+}
+
+async function getPosts(postId) {
+     return await axios.get(`${baseURL}/comments`, {
+        params: {
+            postId: postId
+        },
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+}
+
+describe('API Tests HW 15.1. Error Handling', () => {
+
+    it('1.Error Handling: should handle 404 error when URL is incorrect', async () => {
+        const result = await invalidUrl(baseURL);
+
+        expect(result).toBe(404);
+    });
+
+    it('2.Testing Request Headers and Params', async () => {
+        const testPostId = 1;
+        const response = await getPosts(testPostId);
+
+        expect(response.status).toBe(200);
+        expect(response.config.params.postId).toBe(testPostId);
+        expect(response.config.headers['Content-Type']).toBe('application/json');
+    });
+});
